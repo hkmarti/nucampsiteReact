1386a72ddbd0c1dcd80c8d2c235392062d478a48
Heather Martin <hkoto328@gmail.com>
2021-08-28 13:17:57 -0400

    Week 2 Assignment: Add CampsiteInfo Component

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class CampsiteInfo extends Component {
+
+renderCampsite(campsite){
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+renderComments(comments){
+    if (comments){
+        return(
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                <div>
+                    {comments.map ( comment => 
+                    
+                    <div>
+                         <p>
+                             {comment.text}
+                         <br/>
+                         -- {comment.author} ,  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                         </p>
+
+                    </div>  
+                    )}
+                </div>
+            </div>
+        );   
+    } //Else//
+        return <div/>;
+}
+
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
+            );
+        } //Else//
+            return <div/>;
+    }
+}
+
+export default CampsiteInfo;
+
+
+
