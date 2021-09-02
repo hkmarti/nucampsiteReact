8acd0a8f160d1ca65b2e87fc7a690b12598b6112
Heather Martin <hkoto328@gmail.com>
2021-09-02 18:16:14 -0400

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,14 +4,24 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+
+//Notes: Need to use arrow function instead of function declaration for <Home...> //
+//That's because arrow functions inherit the THIS of their parent scope. //
 
 class Main extends Component {
     constructor(props){
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -19,7 +29,11 @@ class Main extends Component {
 
         const HomePage = () => {
             return(
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         }
 
@@ -29,6 +43,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites ={this.state.campsites}/>} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
@@ -36,5 +51,9 @@ class Main extends Component {
         );
     }
 }
+//If passing along state data, use the render method like with Directory. 
+//If you're not passing state data, use component={NameOfComponent}
+//Make sure to put all Routes BEFORE the Redirect tag otherwise they won't work.
+
 
 export default Main;
