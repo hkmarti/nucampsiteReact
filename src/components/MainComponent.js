278806dd90b4d76fcdf51d1a982934038ad32fb7
Heather Martin <hkoto328@gmail.com>
2021-09-09 16:14:47 -0400

    Intro to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,42 +6,39 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import {Switch, Route, Redirect} from 'react-router-dom';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 //Notes: Need to use arrow function instead of function declaration for <Home...> //
 //That's because arrow functions inherit the THIS of their parent scope. //
 
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
+        promotions: state.promotions
+    };
+};
+
 class Main extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
-        };
-    }
 
     render() {
 
         const HomePage = () => {
             return(
                 <Home
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                    campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
             );
         }
 
         const CampsiteWithId = ({match}) => {
             return (
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                 />
             );
         }
@@ -51,9 +48,9 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites ={this.state.campsites}/>} />
+                    <Route exact path='/directory' render={() => <Directory campsites ={this.props.campsites}/>} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/aboutus' render={() => <About partners ={this.state.partners}/>} />
+                    <Route exact path='/aboutus' render={() => <About partners ={this.props.partners}/>} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
@@ -66,5 +63,4 @@ class Main extends Component {
 //If you're not passing state data, use component={NameOfComponent}
 //Make sure to put all Routes BEFORE the Redirect tag otherwise they won't work.
 
-
-export default Main;
+export default withRouter (connect(mapStateToProps)(Main));
