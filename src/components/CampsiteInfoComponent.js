c2af9034c9c2e2dc84d8e91c0baf8ba952701f20
Heather Martin <hkoto328@gmail.com>
2021-09-11 13:16:03 -0400

    Workshop Assignment 4: Campsiteinfo Modal

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,116 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
-//Presentational Component with 3 functional components//
+//checks if no value(!val) OR value's length is less than or equal to maxlength
+const maxLength = len => val => !val || (val.length <= len);
 
+//checks if there's a value AND value length is greater or equal to minLength
+const minLength = len => val => val && (val.length >=len)
+
+class CommentForm extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        //binds this to toggleModal//
+        //Can use this or rewrite toggleModal() as arrow function which auto-binds//
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    //Toggles Modal on and off//
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    //Receives values from LocalForm and displays as alert & console.log//
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+    } 
+
+    render() {
+        return (
+            
+            <div>
+                {/* Submit Button, opens Model when clicked */}
+                <Button outline onClick={this.toggleModal}>
+                    <i className="fa fa-pencil fa-lg"/> Submit Comment
+                </Button>
+
+                {/* START OF MODAL FORM */}
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    {/* Modal Header, toggle={this.toggleModal} --> enables X button */}
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            {/* Sends input value to handleSubmit() */}
+                            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+
+                                {/*RATING CONTROL*/}
+                                <div className="form-group">
+                                    <Label htmlFor="rating">Rating</Label>
+                                    <Control.select model=".rating" id="rating" name="rating"
+                                        className="form-control"
+                                        defaultValue="1"
+                                    >
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </Control.select>
+                                </div>
+
+                                {/*AUTHOR aka. YOURNAME CONTROL*/}
+                                <div className="form-group">
+                                    <Label htmlFor="author">Your Name</Label>
+                                    <Control.text model=".author" id="author" name="author"
+                                        className="form-control"
+                                        placeholder="Your Name"
+                                        validators={{
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15),
+                                        }}
+                                    />
+                                    <Errors 
+                                            className="text-danger"
+                                            model=".author"
+                                            show="touched"
+                                            component="div"
+                                            messages={{
+                                                minLength: 'Must be at least 2 characters',
+                                                maxLength: 'Must be 15 characters or less'
+                                            }}
+                                        />
+                                </div>
+
+                                {/*COMMENT BOX CONTROL*/}
+                                <div className="form-group">
+                                    <Label htmlFor="text">Comment</Label>
+                                    <Control.textarea model=".text" id="text" name="text" 
+                                        className="form-control"
+                                        row="6"
+                                    />
+                                </div>
+
+                                {/*SUBMIT BUTTON, use type="submit" here bc you're sending data*/}
+                                <Button type="submit" value="submit" color="primary">Submit</Button>
+                                
+                            </LocalForm>
+                    </ModalBody>
+                </Modal>
+                {/*END OF MODAL FORM*/}
+            </div>
+        );
+    }
+}
 
 function RenderCampsite({campsite}){
     return (
@@ -32,10 +139,12 @@ function RenderComments({comments}){
                          <br/>
                          -- {comment.author} ,  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                          </p>
-
                     </div>  
                     )}
                 </div>
+
+                {/*Displays "Submit Comment" button which activates Modal*/}
+                <CommentForm/>
             </div>
         );   
     } //Else//
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -100,7 +100,7 @@ class Header extends Component {
                             </FormGroup>
                               
                             <FormGroup>
-                                <Label htmlFor="password">Username</Label>
+                                <Label htmlFor="password">Password</Label>
                                 <Input type="text" id="password" name="password" 
                                     innerRef={input => this.password = input}/>
                             </FormGroup>
